3dab971aaaf1b897ada7b6eef32dc5d7dd555ffa
1216773121@qq.com <1216773121@qq.com>
2019-06-12 20:52:22 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}123</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -27,7 +27,11 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div>
+      <el-button @click="show = !show">Click Me</el-button>
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -35,7 +39,7 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
 }
 </script>
 
---
+++ src/views/Elm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <el-button @click="show = !show">Click Me</el-button>
+
+    <div style="display: flex; margin-top: 20px; height: 100px;">
+      <transition name="el-fade-in-linear">
+        <div v-show="show" class="transition-box">.el-fade-in-linear</div>
+      </transition>
+      <transition name="el-fade-in">
+        <div v-show="show" class="transition-box">.el-fade-in</div>
+      </transition>
+    </div>
+    <el-button type="text" @click="open">点击打开 Message Box</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: true,
+    methods: {
+      open() {
+        this.$alert("这是一段内容", "标题名称", {
+          confirmButtonText: "确定",
+          callback: action => {
+            this.$message({
+              type: "info",
+              message: `action: ${action}`
+            });
+          }
+        });
+      }
+    }
+  })
+};
+</script>
+
+<style>
+.transition-box {
+  margin-bottom: 10px;
+  width: 200px;
+  height: 100px;
+  border-radius: 4px;
+  background-color: #409eff;
+  text-align: center;
+  color: #fff;
+  padding: 40px 20px;
+  box-sizing: border-box;
+  margin-right: 20px;
+}
+</style>
+
